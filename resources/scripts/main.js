ab9d7c9e8a85e8e6d153bc03d938798583e442c4
jeremydownes <jeremydownes@hotmail.com>
2018-02-23 21:24:06 -0500

    justs one button

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,41 +1,39 @@
 $(document).ready(function() {
-	$('.event').click(function() {
-		$('body').css({backgroundImage: 'url(resources/images/event_loop_no_branch.gif)'})
+	let count = 0
+	$('.button').click(function() {
+		count++
+		switch(count) {
+			case 1:
+				$('body').css({backgroundImage: 'url(resources/images/threadDiverging.gif)'})
+				setTimeout(function() {
+					$('body').css({backgroundImage: 'url(resources/images/threadDivergent.gif)'})
+				},1500)
+				break
+			case 2:
+				$('body').css({backgroundImage: 'url(resources/images/threadConvergence.gif)'})
+				setTimeout(function() {
+					$('body').css({backgroundImage: 'url(resources/images/threadflat.gif)'})
+				},1400)
+				break
+			case 3:
+				$('body').css({backgroundImage: 'url(resources/images/event_loop_no_branch.gif)'})
+				break
+			case 4:
+				$('body').css({backgroundImage: 'url(resources/images/event_loop_no_diversion.gif)'})
+				break
+			case 5:
+				$('body').css({backgroundImage: 'url(resources/images/diversion_loop.gif)'})
+				break
+			case 6:
+				$('body').css({backgroundImage: 'url(resources/images/diversion_step1.gif)'})
+				break
+			case 7:
+				$('body').css({backgroundImage: 'url(resources/images/diversion_step2.gif)'})
+				setTimeout(function() {
+					$('body').css({backgroundImage: 'url(resources/images/diversion_loop.gif)'})
+				},2000)
+				count=0
+				break	
+ 		} 
 	})	
-	$('.timeout').click(function() {
-		$('body').css({backgroundImage: 'url(resources/images/event_loop_no_diversion.gif)'})
-	})
-	$('.divert').click(function() {
-		$('body').css({backgroundImage: 'url(resources/images/diversion_loop.gif)'})
-	})
-	$('.stop').click(function() {
-		$('body').css({backgroundImage: 'url(resources/images/diversion_step1.gif)'})
-	})
-	$('.start').click(function() {
-		$('body').css({backgroundImage: 'url(resources/images/diversion_step2.gif)'})
-		setTimeout(function() {
-			$('body').css({backgroundImage: 'url(resources/images/diversion_loop.gif)'})
-		},2000)
-	})
-
-	$('.thread').click(function() {
-		$('body').css({backgroundImage: 'url(resources/images/threadflat.gif)'})
-	})
-	
-
-	$('.diverge').click(function() {
-		$('body').css({backgroundImage: 'url(resources/images/threadDiverging.gif)'})
-		setTimeout(function() {
-			$('body').css({backgroundImage: 'url(resources/images/threadDivergent.gif)'})
-		},1500)
-	})
-
-
-	$('.converge').click(function() {
-		$('body').css({backgroundImage: 'url(resources/images/threadConvergence.gif)'})
-		setTimeout(function() {
-			$('body').css({backgroundImage: 'url(resources/images/threadflat.gif)'})
-		},1000)
-	})
-
 })
